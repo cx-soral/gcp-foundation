54a598c04ac59b37d5f998f044ef8d2c89e11a71
X-I-A <develop@x-i-a.com>
2024-02-18 23:48:57 +0100

    feat: adding default terraform config

--- iac/environments/base/main.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  landscape = yamldecode(file(var.landscape_file))
-  applications = yamldecode(file(var.applications_file))
-  modules = yamldecode(file(var.modules_file))
-  environment_dict = local.landscape["environments"]
-  modules_dict = local.landscape["modules"]
-  project_prefix = local.landscape["settings"]["project_prefix"]
-}
