275e50aecad86b180e8fdf2afc03bd143ac1f414
X-I-A <develop@x-i-a.com>
2024-02-18 21:23:45 +0100

    feat: adding default terraform config

--- gcp_module_application/templates/gcp-module-application/module/main.tf
+++ gcp_module_application/templates/gcp-module-application/module/main.tf
@@ -20,6 +20,7 @@ locals {
         repository_owner  = app["repository_owner"]
         repository_name   = app["repository_name"]
         project_id        = "${local.project_prefix}${env_name}"
+        match_branch      = env["match_branch"]
       }
     ]
   ]))
@@ -51,4 +52,31 @@ resource "google_iam_workload_identity_pool" "github_pool" {
 
   # Make sure the pool is in a state to be used
   disabled = false
+}
+
+resource "google_iam_workload_identity_pool_provider" "github_provider" {
+  for_each = { for s in local.all_pool_settings : "${s.repository_owner}-${s.repository_name}-${s.project_id}" => s }
+
+  workload_identity_pool_id = google_iam_workload_identity_pool.github_pool[each.key].workload_identity_pool_id
+  workload_identity_pool_provider_id     = "github-provider-${each.value["repository_name"]}"
+  project  = each.value["project_id"]
+
+  # Provider configuration specific to GitHub
+  display_name = "github-provider-${each.value["repository_name"]}"
+  description  = "Provider for GitHub Actions of ${each.value["repository_name"]}"
+
+   # Attribute mapping / condition from the OIDC token to Google Cloud attributes
+  attribute_condition = "assertion.repository == '${each.value["repository_owner"]}/${each.value["repository_name"]}' && assertion.ref.matches('${each.value["match_branch"]}')"
+
+  attribute_mapping = {
+    "google.subject" = "assertion.sub",
+    "attribute.actor" = "assertion.actor",
+    "attribute.repository" = "assertion.repository",
+    "attribute.repository_owner" = "assertion.repository_owner"
+    "attribute.ref" = "assertion.ref"
+  }
+
+  oidc {
+    issuer_uri = "https://token.actions.githubusercontent.com"
+  }
 }
