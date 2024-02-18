0f28507237093e2a9b4dac17513578571d829a4e
X-I-A <develop@x-i-a.com>
2024-02-18 20:37:54 +0100

    feat: adding default terraform config

--- gcp_module_application/templates/gcp-module-application/module/main.tf
+++ gcp_module_application/templates/gcp-module-application/module/main.tf
@@ -13,6 +13,18 @@ locals {
   environment_dict = local.landscape["environments"]
 }
 
+locals {
+  all_pool_settings = toset(flatten([
+    for app_name, app in local.applications : [
+      for env_name, env in local.environment_dict : {
+        repository_owner  = app["repository_owner"]
+        repository_name   = app["repository_name"]
+        project_id        = "${local.project_prefix}${env_name}"
+      }
+    ]
+  ]))
+}
+
 resource "github_repository" "app-repository" {
   for_each = local.applications
 
@@ -25,4 +37,18 @@ resource "github_repository" "app-repository" {
     owner                = "cx-soral"
     repository           = "gcp-framework"
   }
+}
+
+resource "google_iam_workload_identity_pool" "github_pool" {
+  for_each = { for s in local.all_pool_settings : "${s.repository_owner}-${s.repository_name}-${s.project_id}" => s }
+
+  workload_identity_pool_id = "github-pool-${each.value["repository_name"]}"
+  project  = each.value["project_id"]
+
+  # Workload Identity Pool configuration
+  display_name = "github-pool-${each.value["repository_name"]}"
+  description  = "Pool for GitHub Actions of ${each.value["repository_name"]}"
+
+  # Make sure the pool is in a state to be used
+  disabled = false
 }
