dbca8e0691b40c28adf16fd1a4f1373fd87933ee
X-I-A <develop@x-i-a.com>
2024-02-18 19:39:27 +0100

    feat: adding default terraform config

--- gcp_module_project/templates/gcp-module-project/module/main.tf
+++ gcp_module_project/templates/gcp-module-project/module/main.tf
@@ -4,9 +4,15 @@ locals {
   environment_dict = local.landscape["environments"]
 }
 
+data "google_billing_account" "available_accounts" {
+  display_name = "My Billing Account"
+  open         = true
+}
+
 resource "google_project" "env_projects" {
   for_each = local.environment_dict
 
   name = "${local.project_prefix}${each.key}"
   project_id = "${local.project_prefix}${each.key}"
+  billing_account = length(data.google_billing_account.available_accounts.id) > 0 ? data.google_billing_account.available_accounts.id : null
 }
