5786da86e98f317266ed6c40129da08152b45c91
X-I-A <develop@x-i-a.com>
2024-02-18 23:56:45 +0100

    feat: adding default terraform config

--- gcp_module/module.py
+++ gcp_module/module.py
@@ -10,6 +10,7 @@ class Module:
         package_dir = os.path.dirname(os.path.abspath(sys.modules[self.__class__.__module__].__file__))
         self.source_dir = source_dir
         self.module_dir = os.path.join(package_dir, "templates", self.module_name, "module")
+        self.activate_dir = os.path.join(package_dir, "templates", self.module_name, "activate")
         self.base_dir = os.path.join(package_dir, "templates", self.module_name, "base")
         self.base_file = os.path.join(self.base_dir, "main.tf")
         self.activate_file = os.path.join(self.base_dir, "activate.tf")
@@ -47,7 +48,7 @@ class Module:
         if os.path.exists(target_module_dir):
             print(f"Found local module activate-{self.module_name}")
         else:
-            shutil.copytree(self.module_dir, target_module_dir)
+            shutil.copytree(self.activate_dir, target_module_dir)
             print(f"Global module activate-{self.module_name} loaded")
         target_file = os.path.sep.join([base_dir, "activate_" + self.module_name.replace("-", "_") + ".tf"])
         if os.path.exists(target_file):
