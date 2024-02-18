f61062731f3705b85263ed7737d7f501f96d8153
X-I-A <develop@x-i-a.com>
2024-02-18 23:05:38 +0100

    feat: adding default terraform config

--- iac/environments/base/activate_gcp_module_table.tf
+++ iac/environments/base/activate_gcp_module_table.tf
@@ -2,9 +2,9 @@ resource "google_project_iam_custom_role" "gcp_module_table_deployer_role" {
   for_each = local.environment_dict
 
   name = "${local.project_prefix}${each.key}"
-  project_id = "${local.project_prefix}${each.key}"
+  project = "${local.project_prefix}${each.key}"
 
-  role_id     = "gcp_module_table_deployer_role"
+  role_id     = "gcpModuleTableDeployer"
   title       = "GCP Bigquery Table Deployer Role"
   description = "GCP Bigquery Table Deployer Role"
   permissions = [
