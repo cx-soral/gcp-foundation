43fd6e81374bc759e37e8a9c325af8310980c40a
X-I-A <develop@x-i-a.com>
2024-02-23 15:29:14 +0100

    feat: creation of gcp_framework

--- gcp_module_pypi/templates/gcp-module-pypi-gcp/activate/main.tf
+++ gcp_module_pypi/templates/gcp-module-pypi-gcp/activate/main.tf
@@ -109,8 +109,8 @@ resource "google_artifact_registry_repository_iam_member" "gcp_module_python_dep
   for_each = { for s in local.all_role_attribution : "${s.app_name}-${s.env_name}" => s }
 
   project       = each.value["project_id"]
-  location      = google_artifact_registry_repository.pypi_custom.location
-  repository    = google_artifact_registry_repository.pypi_custom.repository_id
+  location      = google_artifact_registry_repository.pypi_custom[each.value["env_name"]].location
+  repository    = google_artifact_registry_repository.pypi_custom[each.value["env_name"]].repository_id
   role          = google_project_iam_custom_role.gcp_module_python_deployer_role[each.value["env_name"]].id
   member        = "serviceAccount:wip-${each.value["app_name"]}-sa@${each.value["project_id"]}.iam.gserviceaccount.com"
 
