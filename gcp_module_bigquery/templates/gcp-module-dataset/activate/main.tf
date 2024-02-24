e4b8195ee9888a5cb14a819870769e1d2e08820e
X-I-A <develop@x-i-a.com>
2024-02-24 15:52:32 +0100

    feat: modules event parameters

--- gcp_module_bigquery/templates/gcp-module-dataset/activate/main.tf
+++ gcp_module_bigquery/templates/gcp-module-dataset/activate/main.tf
@@ -1,5 +1,5 @@
 locals {
-  module_name = substr(basename(dirname(path.module)), 9, length(basename(dirname(path.module))) - 9)
+  module_name = substr(basename(path.module), 9, length(basename(path.module)) - 9)
   landscape = yamldecode(file(var.landscape_file))
   applications = yamldecode(file(var.applications_file))
   project_prefix = local.landscape["settings"]["project_prefix"]
