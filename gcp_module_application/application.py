3cf467943666f2cbd463e58efd4bfd8a9e459f44
X-I-A <develop@x-i-a.com>
2024-02-24 18:18:49 +0100

    feat: gcp module application

--- gcp_module_application/application.py
+++
@@ -1,5 +0,0 @@
-from gcp_module import Module
-
-
-class Application(Module):
-    module_name = "gcp-module-application"
--- gcp_module_application/templates/gcp-module-application/base/main.tf
+++
@@ -1,8 +0,0 @@
-module "gcp_module_application" {
-  source = "../../modules/gcp-module-application"
-
-  landscape_file = "../../../config/landscape.yaml"
-  applications_file = "../../../config/applications.yaml"
-
-  depends_on = [module.gcp_module_project]
-}
