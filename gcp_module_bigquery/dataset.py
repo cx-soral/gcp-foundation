2755e84b4d1bb11baaae345da1685988e1c72905
X-I-A <develop@x-i-a.com>
2024-02-24 18:46:27 +0100

    feat: gcp module bigquery

--- gcp_module_bigquery/dataset.py
+++
@@ -1,5 +0,0 @@
-from gcp_module import Module
-
-
-class Dataset(Module):
-    module_name = "gcp-module-dataset"
--- gcp_module_bigquery/table.py
+++
@@ -1,5 +0,0 @@
-from gcp_module import Module
-
-
-class Table(Module):
-    module_name = "gcp-module-table"
